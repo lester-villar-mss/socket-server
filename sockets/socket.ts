79fe592328cd736e23da84643fb52f82d002ee26
Lester <lester.villar@mss.pe>
2020-05-13 17:43:10 -0500

    Fin Sec 5

--- sockets/socket.ts
+++ sockets/socket.ts
@@ -1,9 +1,20 @@
 import { Socket } from "socket.io";
 import socketIO from 'socket.io';
+import { UsuarioLista } from '../classes/usuarios-lista';
+import { Usuario } from "../classes/usuario";
+
+export const usuariosConectados = new UsuarioLista();
+
+
+export const conectarCliente = ( cliente: Socket ) => {
+    const usuario = new Usuario(cliente.id);
+    usuariosConectados.agregar(usuario);
+};
 
 
 export const desconectar = (cliente: Socket) => {
     cliente.on('disconnect', () => {
+        usuariosConectados.borrarUsuario(cliente.id);
         console.log('Cliente Desconectado');
     });
 }
@@ -14,5 +25,16 @@ export const mensaje = (cliente: Socket, io: socketIO.Server) => {
         console.log('Mensaje recibido', payload);
 
         io.emit('mensaje-nuevo', payload);
+    });
+}
+
+//Configurar Usuario
+export const configurarUsuario = (cliente: Socket, io: socketIO.Server) => {
+    cliente.on('configurar-usuario', (payload: {nombre: string}, callback: Function) => {
+        usuariosConectados.actualizarNombre( cliente.id, payload.nombre);
+        callback({
+            ok: true,
+            mensaje: `Usuario ${ payload.nombre }, configurado`
+        });
     });
 }
